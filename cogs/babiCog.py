168801f10dec48e9ca60b8a9190f1345378b7718
Pavan <pavanvenkatesh94@gmail.com>
2016-11-27 15:48:30 +0530

    BabiCog added

--- cogs/babiCog.py
+++ cogs/babiCog.py
@@ -1,18 +1,17 @@
 import discord
 from discord.ext import commands
 
-class Mycog:
+class BabiCog:
     """My custom cog that does stuff!"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def mycom(self):
+    async def whoareyou(self):
         """This does stuff!"""
-
         #Your code will go here
-        await self.bot.say("I can do stuff!")
+        await self.bot.say("Hi I am Babi. Nice to meet you.")
 
 def setup(bot):
-    bot.add_cog(Mycog(bot))
+    bot.add_cog(BabiCog(bot))
